5b018d9be62c23486b16544960daceaf73d82d07
mohammad-zytoon <mohamadzaiton@gmail.com>
2021-04-11 23:44:52 +0300

    fixed done for the the prev labs

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ console.log(storesPlace);
 let restoruntWorkingTime = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let parent = document.getElementById('parent');
-let container= document.getElementById('parent');
+//let container= document.getElementById('parent');
 
 //
 
